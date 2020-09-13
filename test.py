7dc60c7b3e71d5ddb2bbb57be409bb66f6123039
ramizdundar <ramizdundar@gmail.com>
2020-09-13 05:09:24 +0300

    (Implement) search and borrow functions

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import mysql.connector
+from datetime import datetime
+from datetime import timedelta
 
 # create table books (isbn INT PRIMARY KEY, title VARCHAR(255), author VARCHAR(255), borrowed BOOLEAN);
-# create table borrows (tc INT PRIMARY KEY, isbn INT, date INT);
+# create table borrows (tc INT, isbn INT, date VARCHAR(255));
 
-repopulate = 0
+repopulate = 1
+test_search = 0
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -52,6 +55,48 @@ def flush():
     mydb.commit()
 
 
+def search(value, field):
+    mycursor = mydb.cursor()
+
+    sql = f"SELECT * FROM books WHERE {field} = %s"
+    val = (value, )
+
+    mycursor.execute(sql, val)
+
+    myresult = mycursor.fetchall()
+
+    return myresult
+
+
+def borrow(tc, isbn):
+    book = search(isbn, "isbn")
+
+    if len(book) == 0:
+        print("Book not found")
+        return False
+
+    if book[0][3] == 1:
+        print("Book is not available")
+        return False
+
+    mycursor = mydb.cursor()
+
+    sql = "UPDATE books SET borrowed = %s WHERE isbn = %s"
+    val = (1, isbn)
+
+    mycursor.execute(sql, val)
+
+    sql = "INSERT INTO borrows (tc, isbn, date) VALUES (%s, %s, %s)"
+    val = (tc, isbn, (datetime.now()+timedelta(days=14)).strftime("%d-%m-%Y"))
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(f"TC: {tc} borrowed ISBN: {isbn}")
+    return True
+
+
+
 if repopulate == 1:
     flush()
     for i in range(1000, 2000):
@@ -60,6 +105,15 @@ if repopulate == 1:
     for i in range(1003, 2000, 5):
         delete(i)
 
+    for i in range(1002, 2100, 5):
+        borrow(i*1000,i)
+
+    for i in range(1002, 1102, 5):
+        borrow(i*10,i)
+
+
+if test_search == 1:
+    print(search("ramiz2", "author"))
 
 
 
